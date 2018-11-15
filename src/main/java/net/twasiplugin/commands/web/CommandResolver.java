c600988a5715e418d07c81bfaaafbcd54777c21a
Lars Bärtschi <lars.baertschi@bluewin.ch>
2018-11-15 17:42:33 +0100

    update commands retrieval via GraphQL

--- src/main/java/net/twasiplugin/commands/web/CommandResolver.java
+++ src/main/java/net/twasiplugin/commands/web/CommandResolver.java
@@ -1,41 +1,28 @@
 package net.twasiplugin.commands.web;
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import net.twasi.core.database.models.User;
-import net.twasi.core.logger.TwasiLogger;
+import net.twasi.core.graphql.TwasiCustomResolver;
 import net.twasi.core.services.ServiceRegistry;
 import net.twasi.core.services.providers.DataService;
-import net.twasi.core.services.providers.JWTService;
 import net.twasiplugin.commands.CommandRepository;
-import net.twasiplugin.commands.CustomCommand;
-import net.twasiplugin.commands.web.model.CommandDTO;
+import net.twasiplugin.commands.web.model.CommandPluginDTO;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class CommandResolver implements GraphQLQueryResolver {
+public class CommandResolver extends TwasiCustomResolver {
     private CommandRepository repo = ServiceRegistry.get(DataService.class).get(CommandRepository.class);
 
-    public List<CommandDTO> getCommands(String token) {
-        try {
-            User user = ServiceRegistry.get(JWTService.class).getManager().getUserFromToken(token);
-            try {
-                ServiceRegistry.get(DataService.class).get(net.twasi.core.database.repositories.UserRepository.class).commit(user);
-            } catch (IllegalArgumentException ignored) {}
+    public CommandPluginDTO getCommands(String token) {
+        User user = getUser(token);
 
-            if (user == null) {
-                return null;
-            }
-
-            List<CustomCommand> commands = repo.getAllCommands(user);
-
-            return commands.stream()
-                    .map(cmd -> new CommandDTO(user, cmd.getId().toString(), cmd.getName(), cmd.getContent()))
-                    .collect(Collectors.toList());
-        } catch (Throwable t) {
-            TwasiLogger.log.error("Fatal Error in GraphQL.", t);
+        if (user == null) {
             return null;
         }
+        return null;
+
+        // List<CustomCommand> commands = repo.getAllCommands(user);
+
+        /* return commands.stream()
+                .map(cmd -> new CommandDTO(user, cmd.getId().toString(), cmd.getName(), cmd.getContent()))
+                .collect(Collectors.toList()); */
     }
 
 }
---
+++ src/main/java/net/twasiplugin/commands/web/model/CommandPluginDTO.java
@@ -0,0 +1,27 @@
+package net.twasiplugin.commands.web.model;
+
+import java.util.List;
+
+public class CommandPluginDTO {
+
+    public List<CommandDTO> commands() {
+        return null;
+    }
+
+    public CommandDTO update(String id, String name, String content) {
+        return null;
+    }
+
+    public CommandDTO single(String id) {
+        return null;
+    }
+
+    public CommandDTO delete(String id) {
+        return null;
+    }
+
+    public CommandDTO create(String name, String content) {
+        return null;
+    }
+
+}
